ba07d5219beef6cf89e1eac91915c35b9598cfce
Hisham Mardam-Bey <hisham.mardambey@gmail.com>
2014-03-09 18:14:10 -0400

    Split up dependencies and created producers sub project.

--- project/Build.scala
+++ project/Build.scala
@@ -2,6 +2,7 @@ import sbt._
 import Keys._
 
 object ApplicationBuild extends Build {
+
   override lazy val settings = super.settings ++
     Seq(
       name := "mypipe",
@@ -18,30 +19,56 @@ object ApplicationBuild extends Build {
         "Twitter Repository" at "http://maven.twttr.com/")
     )
 
+  import Dependencies._
+
   val apiDependencies = Seq(
-    "org.scalatest" % "scalatest_2.10" % "2.1.0" % "test",
-    "com.typesafe.akka" %% "akka-actor" % "2.2.3",
-		"com.github.shyiko" % "mysql-binlog-connector-java" % "0.1.0-SNAPSHOT",
-    "commons-lang" % "commons-lang" % "2.6",
-    "com.netflix.astyanax" % "astyanax-core" % "1.56.48",
-    "com.netflix.astyanax" % "astyanax-thrift" % "1.56.48",
-    "com.netflix.astyanax" % "astyanax-cassandra" % "1.56.48",
-		"com.github.mauricio" %% "mysql-async" % "0.2.12"
+    akkaActor,
+    commonsLang,
+    mysqlAsync,
+    mysqlBinlogConnectorJava,
+    scalaTest,
+    typesafeConfig
   )
 
-	val samplesDependencies = apiDependencies
+  val samplesDependencies = Seq(
+    commonsLang,
+    astyanaxCassansra,
+    astyanaxCore,
+    astyanaxThrift,
+    mysqlAsync,
+    mysqlBinlogConnectorJava
+  )
 
-  val runnerDependencies = apiDependencies
+  val runnerDependencies = Seq(
+    typesafeConfig
+  )
+
+  val producersDependencies = Seq(
+    akkaActor,
+    astyanaxCassansra,
+    astyanaxCore,
+    astyanaxThrift,
+    mysqlAsync,
+    typesafeConfig
+  )
 
   lazy val root = Project(id = "mypipe",
-    base = file(".")) aggregate (api, samples, runner)
+    base = file(".")) aggregate (api, producers, samples, runner)
 
   lazy val runner = Project(id = "runner",
     base = file("mypipe-runner"),
     settings = Project.defaultSettings ++ Seq(
       libraryDependencies ++= runnerDependencies
     ) ++ Format.settings
-  ) dependsOn(api, samples)
+  ) dependsOn(api, producers, samples)
+
+
+  lazy val producers = Project(id = "producers",
+    base = file("mypipe-producers"),
+    settings = Project.defaultSettings ++ Seq(
+      libraryDependencies ++= producersDependencies
+    ) ++ Format.settings
+  ) dependsOn(api)
 
   lazy val api = Project(id = "api",
     base = file("mypipe-api"),
@@ -55,7 +82,21 @@ object ApplicationBuild extends Build {
     settings = Project.defaultSettings ++ Seq(
       libraryDependencies ++= samplesDependencies
     ) ++ Format.settings
-  ) dependsOn(api)
+  ) dependsOn(api, producers)
+
+
+object Dependencies {
+
+    val scalaTest = "org.scalatest" % "scalatest_2.10" % "2.1.0" % "test"
+    val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.2.3"
+    val typesafeConfig = "com.typesafe" % "config" % "1.2.0"
+    val mysqlBinlogConnectorJava = "com.github.shyiko" % "mysql-binlog-connector-java" % "0.1.0-SNAPSHOT"
+    val commonsLang = "commons-lang" % "commons-lang" % "2.6"
+    val mysqlAsync = "com.github.mauricio" %% "mysql-async" % "0.2.12"
+    val astyanaxCore = "com.netflix.astyanax" % "astyanax-core" % "1.56.48"
+    val astyanaxThrift = "com.netflix.astyanax" % "astyanax-thrift" % "1.56.48"
+    val astyanaxCassansra = "com.netflix.astyanax" % "astyanax-cassandra" % "1.56.48"
+  }
 }
 
 object Format {
