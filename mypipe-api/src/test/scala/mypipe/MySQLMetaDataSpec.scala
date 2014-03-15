e79a6dcfb37e6d4cd13d083781669a136d81c308
Hisham Mardam-Bey <hisham.mardambey@gmail.com>
2014-03-15 18:31:38 -0400

    The MySQLMetaDataSpec now works. Eventually we should remove the need to hand it column types.

--- mypipe-api/src/test/scala/mypipe/MySQLMetaDataSpec.scala
+++ mypipe-api/src/test/scala/mypipe/MySQLMetaDataSpec.scala
@@ -1,6 +1,6 @@
 package mypipe
 
-import mypipe.mysql.{ MySQLMetadataManager, Listener, BinlogFilePos, BinlogConsumer }
+import mypipe.mysql._
 import scala.concurrent.{ Future, Await }
 import scala.concurrent.duration._
 import mypipe.api._
@@ -13,6 +13,9 @@ import mypipe.api.InsertMutation
 import akka.util.Timeout
 import akka.agent.Agent
 import scala.collection.mutable.ListBuffer
+import mypipe.api.PrimaryKey
+import mypipe.api.ColumnMetadata
+import scala.Some
 
 class MySQLMetaDataSpec extends UnitSpec with DatabaseSpec with ActorSystemSpec with BeforeAndAfterAll {
 
@@ -32,9 +35,28 @@ class MySQLMetaDataSpec extends UnitSpec with DatabaseSpec with ActorSystemSpec 
 
   "MySQLMetadataManager" should "be able to fetch metadata for tables" in {
 
+    implicit val timeout = Timeout(1 second)
+
     val manager = system.actorOf(
       MySQLMetadataManager.props(hostname, port.toInt, username, Some(password)),
       s"TestDBMetadataActor-$hostname:$port")
 
+    val columnTypes: Array[ColumnType.EnumVal] = Array(ColumnType.INT24, ColumnType.VARCHAR, ColumnType.VARCHAR, ColumnType.INT24)
+    val future = ask(manager, GetColumns("mypipe", "user", columnTypes)).asInstanceOf[Future[(List[ColumnMetadata], Option[PrimaryKey])]]
+    val c = Await.result(future, 2 seconds)
+    val columns = c._1
+    val pKey = c._2
+
+    assert(columns(0).name == "id")
+    assert(columns(0).colType == ColumnType.INT24)
+    assert(columns(1).name == "username")
+    assert(columns(1).colType == ColumnType.VARCHAR)
+    assert(columns(2).name == "password")
+    assert(columns(2).colType == ColumnType.VARCHAR)
+    assert(columns(3).name == "login_count")
+    assert(columns(3).colType == ColumnType.INT24)
+    assert(pKey.isDefined == true)
+    assert(pKey.get.columns.size == 1)
+    assert(pKey.get.columns(0).name == "id")
   }
 }
