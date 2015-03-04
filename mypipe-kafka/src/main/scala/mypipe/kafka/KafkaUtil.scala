609b74316195f3f6891b0297124b0005dca99130
Hisham Mardam-Bey <hisham.mardambey@mate1inc.com>
2015-03-04 16:27:14 -0500

    Fetch topic names for generic / specific producers from config.

--- mypipe-kafka/src/main/scala/mypipe/kafka/KafkaUtil.scala
+++ mypipe-kafka/src/main/scala/mypipe/kafka/KafkaUtil.scala
@@ -2,6 +2,7 @@ package mypipe.kafka
 
 import com.typesafe.config.ConfigFactory
 import mypipe.api.event.Mutation
+import mypipe.util.Eval
 
 object KafkaUtil {
 
@@ -9,16 +10,19 @@ object KafkaUtil {
   val genericTopicFormat = config.getString("mypipe.kafka.generic-producer.topic-format")
   val specificTopicFormat = config.getString("mypipe.kafka.specific-producer.topic-format")
 
+  val generictplFn = Eval[(String, String) ⇒ String]("{ (db: String, table: String) => { s\"" + genericTopicFormat + "\" } }")
+  val specifictplFn = Eval[(String, String) ⇒ String]("{ (db: String, table: String) => { s\"" + specificTopicFormat + "\" } }")
+
   def genericTopic(mutation: Mutation[_]): String =
     genericTopic(mutation.table.db, mutation.table.name)
 
   def genericTopic(db: String, table: String) =
-    s"${db}_${table}_generic"
+    generictplFn(db, table)
 
   def specificTopic(mutation: Mutation[_]): String =
     specificTopic(mutation.table.db, mutation.table.name)
 
   def specificTopic(db: String, table: String): String =
-    s"${db}_${table}_specific"
+    specifictplFn(db, table)
 }
 
