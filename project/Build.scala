6dadb3907f5873aea58bc4f8a5755f9108e97ce3
Hisham Mardam-Bey <hisham.mardambey@gmail.com>
2015-06-19 15:27:56 -0400

    Move up to mysql-binlog-connector-java 0.2.1.

--- project/Build.scala
+++ project/Build.scala
@@ -116,7 +116,7 @@ object Dependencies {
   val kafka = "org.apache.kafka" % "kafka_2.11" % "0.8.2-beta" exclude("javax.jms", "jms") exclude("com.sun.jdmk", "jmxtools") exclude("com.sun.jmx", "jmxri")
   val logback = "ch.qos.logback" % "logback-classic" % "1.1.1"
   val mysqlAsync =  "com.github.mauricio" % "mysql-async_2.11" % "0.2.15"
-  val mysqlBinlogConnectorJava = "com.github.shyiko" % "mysql-binlog-connector-java" % "0.1.1"
+  val mysqlBinlogConnectorJava = "com.github.shyiko" % "mysql-binlog-connector-java" % "0.2.1"
   val scalaCompiler = "org.scala-lang" % "scala-compiler" % "2.11.2"
   val scalaReflect = "org.scala-lang" % "scala-reflect" % "2.11.2"
   val scalaTest = "org.scalatest" %% "scalatest" % "2.2.1" % "test"
